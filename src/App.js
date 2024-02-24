6c93e695710ebf5707700d4351f29f7fce67eed5
oujii <carl.waerner@hyperisland.se>
2024-02-24 12:23:19 +0100

    firsta12

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import './App.css';
 import MyDiv from './MyDiv';
 import VideoCapture from './VideoCapture';
+import tallImage from './images/stories.png';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ display: 'flex' }}>
-          <div style={{ width: '50%' }}>
-            <MyDiv />
-          </div>
-
-          <div style={{ width: '100%'}}>
-            <VideoCapture />
-          </div>
-          </div>
-
-        
-      </header>
+    <div className="container">
+      <div className="column1">
+      <div className="column1-top"><img src={tallImage} /></div>
+      <div className="column1-bottom">bottom 80%ss</div>
     </div>
+
+    <div className='column2'>
+      <VideoCapture />
+      <MyDiv />
+    </div>
+  </div>
+
   );
 }
 
