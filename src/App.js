4c2ae38de17a47503a78bbe03de663cd545095d9
oujii <carl.waerner@hyperisland.se>
2024-02-24 13:12:14 +0100

    fullscreen grejer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import MyDiv from './MyDiv';
 import VideoCapture from './VideoCapture';
 import tallImage from './images/stories.png';
+import scrollImage from './images/feed_scroll.png';
 import React, { useState } from 'react';
 
 
@@ -10,11 +11,11 @@ function App() {
   function toggleFullscreen() {
     if (!document.fullscreenElement) {
       document.documentElement.requestFullscreen();
-      setIsFullscreen(true);
+
     } else {  
       if (document.exitFullscreen) {
         document.exitFullscreen(); 
-        setIsFullscreen(false);
+
       }
     }
   }
@@ -23,7 +24,7 @@ function App() {
     <div className="container">
       <div className="column1">
       <div className="column1-top"><a onClick={toggleFullscreen}><img className="stories" src={tallImage} alt='test'/></a></div>
-      <div className="column1-bottom">hejsan</div>
+      <div className="column1-bottom"><img className="scroll" src={scrollImage} alt='test'/></div>
     </div>
 
     <div className='column2'>
